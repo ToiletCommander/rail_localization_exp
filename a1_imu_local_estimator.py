0065a22f8a5e0b31d7760d5a5d25b73aea404cb3
Coding Cookie <1745500559@qq.com>
2022-09-20 13:47:33 -0700

    Update a1_imu_local_estimator.py

--- a1_imu_local_estimator.py
+++ a1_imu_local_estimator.py
@@ -1,7 +1,7 @@
 import time
 import typing
 import openvr
-from localizer_base import LocalFrameEstimatorImpl, NonBlocking, rotation_angle_from_quaternion, rotation_matrix, rotation_matrix_inverse
+from localizer_base import GlobalFrameEstimatorImpl, LocalFrameEstimatorImpl, NonBlocking, rotation_angle_from_quaternion, rotation_matrix, rotation_matrix_inverse
 import numpy as np
 from robot_interface import RobotInterface # pytype: disable=import-error
 import inspect
@@ -43,94 +43,93 @@ from_a1_frame_mat = np.array([
 
 to_a1_frame_mat = from_a1_frame_mat
 
-class A1RobotIMULocalEstimator(LocalFrameEstimatorImpl, NonBlocking):
+class A1RobotIMULocalEstimator(GlobalFrameEstimatorImpl, NonBlocking):
     def __init__(self, robot_interface : typing.Optional[RobotInterface]):
-        LocalFrameEstimatorImpl.__init__(
+        GlobalFrameEstimatorImpl.__init__(
             self,
-            "A1RobotIMULocalEstimator",
+            "A1RobotIMUEstimator",
+            autoUpdateLocation=False,
             autoUpdateVelocity=False,
-            autoUpdateAcceleration=False
+            autoUpdateAcceleration=False,
+            autoUpdateLocalVelocity=False,
+            autoUpdateLocalAcceleration=False
         )
         self.robot_interface = robot_interface
 
-        self.__lastLocalAngularLocation : np.ndarray = None
-        self.__lastLocalAngularLocationUpdate : float = 0
+        self._lastGlobalAngularLocation : np.ndarray = None
+        self._lastGlobalAngularLocationUpdate : float = 0
 
-        self.__lastLocalLinearVelocity : np.ndarray = None
-        self.__lastLocalLinearVelocityUpdate : float = 0
+        self._lastGlobalLinearVelocity : np.ndarray = None
+        self._lastGlobalLinearVelocityUpdate : float = 0
+
+        self._lastGlobalAcceleration : np.ndarray = None
+        self._lastGlobalAccelerationUpdate : float = 0
+
+        self.negative_gravity_vector = np.array([0,0,-9.8])
+
+        self.update()
     
+    def getAngularLocation(self) -> np.ndarray:
+        return self._lastLocalAngularLocation()
+        
+    def calibrate_for_gravity(self, duration_to_take_mean) -> None:
+        start_time = time.time()
+        sum_of_gravity = np.array([0,0,0])
+        last_time = start_time
+        while True:
+            ctime = time.time()
+            dt = ctime - last_time
+            if ctime - start_time > duration_to_take_mean:
+                break
+
+
+            observation = self.robot_interface.receive_observation()
+
+            q = observation.imu.quaternion
+            accelerometer_reading = np.array(observation.imu.accelerometer)
+            
+            if np.all(accelerometer_reading == 0):
+                raise Exception("Accelerometer reading is 0,0,0. Is the IMU connected?")
+            
+            rot_ang = rotation_angle_from_quaternion(q)
+            rot_mat = rotation_matrix(*rot_ang)
+            accelerometer_reading_global = rot_mat @ accelerometer_reading
+            sum_of_gravity += accelerometer_reading_global * dt
+
+        gravity = sum_of_gravity / duration_to_take_mean
+        self.negative_gravity_vector = -gravity
+
     def update(self) -> None:
         if self.robot_interface is not None:
             self.updateFromObservation(self.robot_interface.receive_observation())
-    
+
     def updateFromObservation(self, observation : typing.Any) -> None:
         #wxyz quaternion
         q = observation.imu.quaternion
 
         #xyz acceleration in original coordinates
-        accelerometer_reading = np.array(observation.imu.accelerometer)
+        raw_accelerometer_reading = np.array(observation.imu.accelerometer)
 
-        """
-        _motor_angles = np.array(
-            [motor.q for motor in state.motorState[:12]])
-        _motor_velocities = np.array(
-            [motor.dq for motor in state.motorState[:12]])
-        _joint_states = np.array(
-            list(zip(_motor_angles, _motor_velocities)))
-        _observed_motor_torques = np.array(
-            [motor.tauEst for motor in state.motorState[:12]])
-        _motor_temperatures = np.array(
-            [motor.temperature for motor in state.motorState[:12]])
-        """
-        rot_ang = rotation_angle_from_quaternion(q)
-        inv_rot_mat = rotation_matrix_inverse(*rot_ang)
+        raw_rot_ang = rotation_angle_from_quaternion(q)
+        raw_inv_rot_mat = rotation_matrix_inverse(*raw_rot_ang)
 
 
-        calibrated_accelerometer_reading = accelerometer_reading + inv_rot_mat @ np.array([0,0,-9.8])
+        calibrated_accelerometer_raw_reading = raw_accelerometer_reading + raw_inv_rot_mat @ self.negative_gravity_vector
         
-        if np.all(accelerometer_reading == 0):
-            calibrated_accelerometer_reading = np.zeros((3,))
-            rot_ang = np.zeros((3,))
+        if np.all(raw_accelerometer_reading == 0):
+            calibrated_accelerometer_raw_reading = np.zeros((3,))
+            raw_rot_ang = np.zeros((3,))
+            return
 
 
         #print(calibrated_accelerometer_reading)
 
-        transformed_rot_ang = from_a1_frame_mat @ rot_ang
-        transformed_calibrated_accelerometer_reading = from_a1_frame_mat @ calibrated_accelerometer_reading
+        rot_ang_global = from_a1_frame_mat @ raw_rot_ang
+        accelerometer_reading_local = from_a1_frame_mat @ calibrated_accelerometer_raw_reading
+        rot_mat_to_global = rotation_matrix(*rot_ang_global)
+        accelerometer_reading_global = rot_mat_to_global @ accelerometer_reading_local
 
-        ctime = time.time()
-
-        linear_velocity = None
-
-        if self.__lastLocalLinearVelocityUpdate == 0:
-            self.__lastLocalLinearVelocityUpdate = ctime
-            self.__lastLocalLinearVelocity = np.zeros(3)
-        else:
-            linear_velocity_dt = ctime - self.__lastLocalLinearVelocityUpdate
-            linear_velocity = self.__lastLocalLinearVelocity + transformed_calibrated_accelerometer_reading * linear_velocity_dt
-            self.__lastLocalLinearVelocity = linear_velocity
-            self.__lastLocalLinearVelocityUpdate = ctime
-
-        angular_velocity = None
-        # ehhhh - how to estimate angular velocity from global position reading?
-        # TODO: Revisit this
-        if self.__lastLocalAngularLocationUpdate == 0:
-            self.__lastLocalAngularLocationUpdate = ctime
-            self.__lastLocalAngularLocation = np.zeros(3)
-        else:
-            angular_velocity_dt = ctime - self.__lastLocalAngularLocationUpdate
-            angular_velocity = (transformed_rot_ang - self.__lastLocalAngularLocation) / angular_velocity_dt
-            self.__lastLocalAngularLocation = transformed_rot_ang
-            self.__lastLocalAngularLocationUpdate = ctime
-
-        # update all accelerations
-        if self._lastLocalVelocityUpdate != 0:
-            last_angular_velocity = self._lastLocalVelocity[3:]
-            angular_accel = (angular_velocity - last_angular_velocity) / (ctime - self._lastLocalVelocityUpdate)
-            self._call_local_acceleration_update(np.concatenate([transformed_calibrated_accelerometer_reading,angular_accel]).reshape((6,)))
         
-        if linear_velocity is not None and angular_velocity is not None:
-            self._call_local_velocity_update(np.concatenate([linear_velocity,angular_velocity]).reshape((6,)))
-
+        
 
 
