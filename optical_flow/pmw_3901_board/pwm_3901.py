f881fdedffec5eb2e52f0abfe576d819a49f08c2
Windy Feng <1745500559@qq.com>
2022-11-29 19:01:16 -0800

    Update PWM3901

--- optical_flow/pmw_3901_board/pwm_3901.py
+++ optical_flow/pmw_3901_board/pwm_3901.py
@@ -16,6 +16,7 @@ class PWM3901HardwareEstimator(LocalFrameEstimatorImpl, NonBlocking, Parallel):
         serial_baudrate : int = 115200, 
         parallel : bool = False, 
         parallel_wait_time : float = 0.02,
+        serial_wait_time : float = 0.02,
         sliding_window_filter : typing.Optional[ContinuousMovingWindowFilter] = None,
         dist_cm_sliding_window : typing.Optional[ContinuousMovingWindowFilter] = None
     ) -> None:
@@ -28,6 +29,7 @@ class PWM3901HardwareEstimator(LocalFrameEstimatorImpl, NonBlocking, Parallel):
         self.x_multiplier = x_multiplier
         self.y_multiplier = y_multiplier
         self.last_hardware_dt = 0.0
+        self.serial_wait_time = serial_wait_time
 
         self.update()
         
@@ -43,7 +45,13 @@ class PWM3901HardwareEstimator(LocalFrameEstimatorImpl, NonBlocking, Parallel):
         else:
             return dist_cm
     
-    
+    def getEstimatedVelocity(self, obs_d : np.ndarray, obs_v : np.ndarray, obs_dt : float) -> np.ndarray:
+        if self.sliding_window_filter is not None:
+            self.sliding_window_filter.add_observation(obs_dt, obs_v, obs_d)
+            return self.sliding_window_filter.get_per_size_average()
+        else:
+            return obs_v
+
     def processLine(self, line : str, update_v : bool = False) -> None:
         if line == "":
             return
@@ -74,27 +82,24 @@ class PWM3901HardwareEstimator(LocalFrameEstimatorImpl, NonBlocking, Parallel):
         obs_v = obs_d / obs_dt
 
         self.last_hardware_dt = obs_dt
-
-        if self.sliding_window_filter is not None:
-            self.sliding_window_filter.add_observation(obs_dt, obs_v, obs_d)
-
+        estimated_velocity = self.getEstimatedVelocity(obs_d, obs_v, obs_dt)
+        
         if update_v:
-            if self.sliding_window_filter is not None:
-                estimated_velocity = self.sliding_window_filter.get_per_size_average()
-            else:
-                estimated_velocity = obs_v
-            
             self._call_local_velocity_update(np.append(estimated_velocity, np.zeros(4)))
 
     def update(self) -> None:
         if self.ser.isOpen():
             self.ser.write(b'\x02') #write arbitrary byte
             self.ser.flush()
+            
+            st = time.time()
+            serial_raw = self.ser.read_all().decode("ascii") #self.ser.readline()
+            while(not(serial_raw.endswith('\n')) and  time.time() - st < self.serial_wait_time):
+                serial_raw += self.ser.read_all().decode("ascii")
+            
+            lines = serial_raw.split("\n")
 
-            lines = self.ser.read_all().decode("ascii").split("\n") #self.ser.readline()
-
-            while len(lines) > 0 and lines[-1].strip() == "":
-                lines = lines[:-1]
+            lines.pop()
             
             for idx, line in enumerate(lines):
                 self.processLine(line, update_v=(idx == len(lines) - 1))
